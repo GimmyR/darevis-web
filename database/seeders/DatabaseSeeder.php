7db43d92a1924844d19f3d84db9283fe605fe51d
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-10-18 09:07:22 +0300

    Seeding database with one user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,23 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+    public function run(): void {
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        /*User::create([
+            "name" => "gimmyraz",
+            "email" => "gimmyarazafimbelo2@gmail.com",
+            "password" => Hash::make("mdpGimmy")
+        ]);*/
+        
     }
 }
