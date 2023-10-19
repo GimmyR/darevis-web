a744196f3d02661e21c05dc928ae99f65c9305b8
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-10-19 10:57:09 +0300

    Creating controller and view for Home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Record;
+use App\Models\User;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index(): View {
 
-        return view("home");
+        /** @var User $user */
+        $user = Auth::user();
+        $records = Record::where("user_id", $user->id)->get();
+
+        return view("home", [
+            "records" => $records
+        ]);
 
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,29 @@
 @extends("base")
 
 @section("content")
-    <p>Home is here !</p>
+    @include("navbar")
+    <div class="container d-flex flex-row justify-content-center mt-3">
+        <div class="col-12 col-sm-12 col-md-10 col-lg-6">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Record</th>
+                        <th scope="col">Creation date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($records as $record)
+                        <tr>
+                            <th>{{ $record->id }}</th>
+                            <td>
+                                <a href="#" class="text-decoration-none">{{ $record->title }}</a>
+                            </td>
+                            <td>{{ $record->created_at->format("d M y - h:i") }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
