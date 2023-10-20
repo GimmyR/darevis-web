61020e1ae60546ea605f74de4588491a77376c89
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-10-20 16:13:03 +0300

    Creating table component for Entry Details

---
+++ resources/js/Pages/components/DetailsTable.vue
@@ -0,0 +1,21 @@
+<script setup>
+    defineProps({ record: Object });
+</script>
+
+<template>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="row" v-for="parameter in record.parameters">
+                    {{ parameter.title }}<span v-if="parameter.unit != null"> ({{ parameter.unit }})</span>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="entry in record.entries">
+                <td v-for="detail in entry.entry_details">{{ detail.value }}</td>
+                <td><a :href="'/entry/' + entry.id + '/edit'">Edit</a></td>
+            </tr>
+        </tbody>
+    </table>
+</template>
