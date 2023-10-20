6571f8c95fd4c0b8e29e5125642f54c6f9d458a0
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-10-20 10:38:53 +0300

    Installing Inertia and Vue

---
+++ resources/js/Pages/Record.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <p>RECORD IS HERE !</p>
+</template>
+
+<style>
+
+</style>
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/scss/app.scss', 'resources/js/app.js'],
+            input: [
+                'resources/scss/app.scss',
+                'resources/scss/appv.scss', 
+                'resources/js/app.js', 
+                'resources/js/appv.js'
+            ],
+            
             refresh: true,
         }),
     ],
