39e1d262fe7cbdfd3a51df5ef5bf84c82bd54c35
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-10-23 08:34:58 +0300

    Adding 'Add Entry' button

--- resources/js/Pages/Record.vue
+++ resources/js/Pages/Record.vue
@@ -10,15 +10,18 @@
     <Navbar/>
     <div class="container d-flex flex-column align-items-center">
         <div class="d-flex flex-row align-items-center">
+            <a href="#" title="Add Entry">
+                <i class="bi bi-plus-circle fs-3 me-3"></i>
+            </a>
             <h1 class="mt-5 mb-5 me-3">{{ record.title }}</h1>
             <a href="#">
                 <i class="bi bi-pencil-square fs-3"></i>
             </a>
         </div>
-        <div class="col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8 mb-5">
+        <div class="col-12 col-sm-12 col-md-10 col-lg-8 col-xl-8 mb-5">
             <DetailsCharts :record="record"/>
         </div>
-        <div class="col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8">
+        <div class="col-12 col-sm-12 col-md-10 col-lg-8 col-xl-8">
             <DetailsTable :record="record"/>
         </div>
     </div>
