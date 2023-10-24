ad156a3e563b76a9b5f87748e36daf23e686ea14
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-10-24 09:27:31 +0300

    Adding relationship for Entry

--- app/Models/Entry.php
+++ app/Models/Entry.php
@@ -15,6 +15,12 @@ class Entry extends Model
         "updated_at"
     ];
 
+    public function record() {
+
+        return $this->belongsTo(Record::class);
+
+    }
+
     public function entryDetails() {
 
         return $this->hasMany(EntryDetail::class);
