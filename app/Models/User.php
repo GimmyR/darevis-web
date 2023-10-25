0258801c266629b5a03e3add3984a0d0f317802b
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-10-25 07:46:09 +0300

    Adding records attribute

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function records() {
+
+        return $this->hasMany(Record::class);
+
+    }
 }
