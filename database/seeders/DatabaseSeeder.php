6cfc07586ef8b2981543852c3c4cade09bcc0692
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-10-26 08:04:22 +0300

    Editing seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\Parameter;
-use App\Models\Record;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -18,19 +16,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void {
 
         User::create([
-            "name" => "gimmyraz",
-            "email" => "gimmyarazafimbelo2@gmail.com",
-            "password" => Hash::make("mdpGimmy")
-        ]);
-
-        Record::create([
-            "user_id" => 1,
-            "title" => "My Health"
-        ]);
-
-        $this->call([
-            ParameterSeeder::class,
-            EntrySeeder::class
+            "name" => "johndoe",
+            "email" => "johndoe@example.com",
+            "password" => Hash::make("mdpJohn")
         ]);
         
     }
