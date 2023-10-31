685c15da7b03bdfefdfb2664960b9e59758dff72
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-10-31 09:13:18 +0300

    Adding horizontal overflow for table

--- resources/js/Pages/components/DetailsTable.vue
+++ resources/js/Pages/components/DetailsTable.vue
@@ -3,20 +3,28 @@
 </script>
 
 <template>
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                <th scope="row" v-for="parameter in parameters">
-                    {{ parameter.title }}<span v-if="parameter.unit != null"> ({{ parameter.unit }})</span>
-                </th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="entry in entries">
-                <td v-for="detail in entry.entry_details">{{ detail.value }}</td>
-                <td><a :href="'/edit-entry/' + entry.id">Edit</a></td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="tableWrapper">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="row" v-for="parameter in parameters">
+                        {{ parameter.title }}<span v-if="parameter.unit != null"> ({{ parameter.unit }})</span>
+                    </th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="entry in entries">
+                    <td v-for="detail in entry.entry_details">{{ detail.value }}</td>
+                    <td><a :href="'/edit-entry/' + entry.id">Edit</a></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
+
+<style>
+    .tableWrapper {
+        overflow-x: scroll;
+    }
+</style>
